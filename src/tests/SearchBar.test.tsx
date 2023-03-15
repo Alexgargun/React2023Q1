141a21e1c094eab28e948e99795601422013f5ff
Alexgagrun <aleksandr.gargun@gmail.com>
2023-03-15 01:16:43 +0100

    feat: add test files for SearchBar

---
+++ src/tests/SearchBar.test.tsx
@@ -0,0 +1,15 @@
+import { render, fireEvent } from "@testing-library/react";
+import SearchBar from "../components/SearchBar";
+
+describe("SearchBar component", () => {
+  it("should render the search bar with the initial value and clear the input on click", () => {
+    const { getByPlaceholderText } = render(<SearchBar />);
+    const input = getByPlaceholderText("search");
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue("");
+
+    fireEvent.change(input, { target: { value: "test" } });
+    expect(input).toHaveValue("test");
+  });
+});
